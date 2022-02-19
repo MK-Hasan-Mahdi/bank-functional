c0a0b8ace3c24c078f5a8b9e7951b1e4b702a707
mk-hasan <mkhasan6891@gmail.com>
2022-02-19 12:00:10 +0600

    getInnerTextValue function added cz withdraw amount can't bigger than balance amount

--- js/functional.js
+++ js/functional.js
@@ -6,6 +6,13 @@ function getInputValue(fieldId) {
     return value; // value ke return kora hoyechey karon ei value ta ke abar new jaigai kaje lagate hobe
 }
 
+function getInnerTextValue(fieldId) {
+    const fieldTag = document.getElementById(fieldId);
+    const fieldValueInText = fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
+}
+
 function totalValueUpdate(fieldId, amount) {
     const totalField = document.getElementById(fieldId); // id diye field ta dhore anbe
     const totalInText = totalField.innerText; // innertext er vitorer value/text ke variable er vitore rakha
@@ -14,25 +21,36 @@ function totalValueUpdate(fieldId, amount) {
     totalField.innerText = newTotalValue; // totalfield er man ke newly define kora holo
 }
 
-function balanceUpdate(amount) {
+function balanceUpdate(amount, isAdding) {
     const balanceTag = document.getElementById('balance-total');
     const balanceInText = balanceTag.innerText;
     const previousBalanceValue = parseFloat(balanceInText);
-    const newBalance = previousBalanceValue + amount;
+    let newBalance;
+    if (isAdding == true) {
+        newBalance = previousBalanceValue + amount;
+    } else {
+        newBalance = previousBalanceValue - amount;
+    }
+
     balanceTag.innerText = newBalance;
 
 }
 // deposit 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input'); // amount variable er maddhome totalValueUpdate function er parameter er man set kora hoyechey 
-    totalValueUpdate('deposit-total', amount); // upor thake amount variable ke paremeter hishabe pathano hoyechey
-    balanceUpdate(amount);
+    if (amount > 0) {   // amount variable ke condition er vitore niye asha hoyechey-ete kore 0 er chaite kom value input deya jabe na, minus figure, text input deya jabe na
+        totalValueUpdate('deposit-total', amount); // upor thake amount variable ke paremeter hishabe pathano hoyechey
+        balanceUpdate(amount, true);
+    }
 })
 // withdraw
 document.getElementById('withdraw-button').addEventListener
     ('click', function () {
         const amount = getInputValue('withdraw-input');
-        totalValueUpdate('withdraw-total', amount)
+        if (amount > 0) {
+            totalValueUpdate('withdraw-total', amount);
+            balanceUpdate(amount, false);
+        }
     })
 
 
